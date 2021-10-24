488601a5fdef63cb3eb581e33ec463c7f9e864f3
大谷学 <77899736+ooyamanabu@users.noreply.github.com>
2021-10-25 07:16:17 +0900

    上書きjs２

--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ function calcSum(num1, num2) {
 }
 const calclatedNum = calcSum(1, 3);
 console.log(calclatedNum);
+
+const trueFlag = true;
+const falseFlag = false;
+
+trueFlag && console.log("A");
+falseFlag && console.log("B");
+trueFlag || console.log("C");
+falseFlag || console.log("D");
