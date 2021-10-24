f0dcc12e367a6fbd335eb342ae92c7b097166bf0
大谷学 <77899736+ooyamanabu@users.noreply.github.com>
2021-10-24 12:31:40 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function sayHello() {
+  console.log("こんにちは！");
+}
+sayHello();
+
+function calcSum(num1, num2) {
+  return num1 + num2;
+}
+const calclatedNum = calcSum(1, 3);
+console.log(calclatedNum);
