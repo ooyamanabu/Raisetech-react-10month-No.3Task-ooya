341df07d4494193c64f34e95851fb3e9f9e48479
大谷学 <77899736+ooyamanabu@users.noreply.github.com>
2021-10-26 05:04:15 +0900

    上書きjs2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-//挨拶
+//第3回reactコース課題
+
+//変数宣言
+const myName = "ooya";
+const yourName = "大谷";
+
+myName = "ooya丸"; //constは定数の為再代入(変更)できない
+
+let iName = "manabu";
+iName = "manabu丸"; //再代入できる
+
+let iName = "manabu君"; //再宣言できない
+
+var mName = "学";
+mName = "学丸"; //エラーなし
+var mName = "学君"; //エラーなし
+//使い勝手が良い反面セキュリティ部分に難有り(書き変わってしまったり、バグの温床)の為今後は使用していかない方が良いとされている
+
+//Javascript基本
+
 function sayHello() {
   console.log("こんにちは！");
 }
